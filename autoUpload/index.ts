f9b27c95d6c1af4858ac6b519614ec6245f201a7
hyyyh <2452719312@qq.com>
2023-04-05 13:35:27 +0800

    将自动部署工具稍微模块化了一下

--- autoUpload/index.ts
+++ autoUpload/index.ts
@@ -4,6 +4,7 @@ const compress = require('./compressFile.ts');
 const [connect, SSH] = require('./ssh.ts')
 const uplod = require('./uploadFile.ts')
 const runcommand = require('./handleCommand.ts')
+const Config = require('./config.ts')
 const program = new Command();
 
 const main = async () => {
@@ -18,18 +19,6 @@ const main = async () => {
     program.requiredOption('-i, --commitInfo <filename>', 'commitInfo filename');
     program.parse();
     let info = program.opts().commitInfo
-    const config = {
-        sshinfo: {
-            host: '47.96.151.77',
-            username: 'Administrator',
-            password: 'Hyyyh1527',
-            // privateKey: "C:/Users/huang'rui/.ssh/id_rsa", // ssh私钥(不使用此方法时请勿填写， 注释即可)
-            // passphrase: '' // ssh私钥对应解密密码(不存在设为''即可)
-        },
-        targetFile: 'blog.zip', // 目标文件
-        deployDir: './Desktop/nginx-1.22.0/html/', // 远端目录
-        nginxDir: 'C:/Users/Administrator/Desktop/nginx-1.22.0'  //nginx目录
-    }
     try {
         console.log("开始打包项目");
         await shell.exec("npm run build-prod")
@@ -44,27 +33,22 @@ const main = async () => {
         await shell.exec("git push")
         console.log("代码上传成功！");
         console.log("开始压缩为zip");
-        await compress('E:/desk/blog/blog/program/blog/dist', __dirname + '/blog.zip')  //压缩
+        await compress('E:/desk/blog/blog/program/blog/dist', __dirname + '/'+ Config.targetFile)  //压缩
         console.log("压缩完成！");
         console.log('正在连接远程服务器');
-        await connect(config.sshinfo)  //连接
+        await connect(Config.sshinfo)  //连接
         console.log("连接成功！");
         console.log("上传压缩包");
-        await uplod(SSH, config, __dirname + '/blog.zip')  //上传压缩包
+        await uplod(SSH, Config, __dirname + '/'+Config.targetFile)  //上传压缩包
         console.log("上传成功！");
         console.log("正在执行解压等后续操作");
         await runcommand(SSH, `autodeploy.bat`, '')  //
         console.log("执行完毕！");
-        // await runcommand(SSH,`7z x blog.zip`,"")  //解压
-        // console.log("解压成功！");
     } catch (error) {
         console.log(error);
     } finally {
         process.exit()
     }
-
-
 }
-
 main()
 
